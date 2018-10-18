85e2f8937ff52cd0f6deab9dc2398076eaf26867
Bavo Van Craeyenest <bavo.vancraeyenest@student.odisee.be>
2018-10-18 08:47:48 +0200

    Labo 2a

---
+++ Labo2A/src/labo2a/SortedStream.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labo2a;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author Bavo
+ */
+public class SortedStream {
+    
+    public Supplier<Stream<Student>> studentenMaker(){
+        Supplier<Stream<Student>> sup = 
+    }
+}
