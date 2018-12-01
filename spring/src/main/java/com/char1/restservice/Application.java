13c3a3b2d46b343ac23fad0dd10c1847307a4f84
Robin Wijnant <robin.wijnant@student.odisee.be>
2018-12-01 20:45:33 +0100

    import SpringBootServletInitializer

--- spring/src/main/java/com/char1/restservice/Application.java
+++ spring/src/main/java/com/char1/restservice/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support;
 
 @SpringBootApplication // SpringBootServletInitializer is required for Tomcat deploy
 public class Application extends SpringBootServletInitializer {
