73bf64ff56faef239031231f00d956e108362ec1
Robin Wijnant <robin.wijnant@student.odisee.be>
2018-12-01 21:26:46 +0100

    Fix import

--- spring/src/main/java/com/char1/restservice/Application.java
+++ spring/src/main/java/com/char1/restservice/Application.java
@@ -4,10 +4,9 @@ import com.char1.restservice.entity.User;
 import com.char1.restservice.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication // SpringBootServletInitializer is required for Tomcat deploy
 public class Application extends SpringBootServletInitializer {
