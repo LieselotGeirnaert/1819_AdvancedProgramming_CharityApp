ca94455928e261e4695a99c053036632bb28ed5d
Robin Wijnant <robin.wijnant@student.odisee.be>
2018-12-01 20:42:26 +0100

    Add spring boot initializer for tomcat

--- spring/src/main/java/com/char1/restservice/Application.java
+++ spring/src/main/java/com/char1/restservice/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication // SpringBootServletInitializer is required for Tomcat deploy
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
