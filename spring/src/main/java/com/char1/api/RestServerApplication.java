5c9dddfb3204472f7a1070e40ff27c6d323dbaa2
Robin Wijnant <robin.wijnant@student.odisee.be>
2018-12-02 14:21:20 +0100

    Updated application class for war

--- spring/src/main/java/com/char1/api/RestServerApplication.java
+++ spring/src/main/java/com/char1/api/RestServerApplication.java
@@ -5,11 +5,17 @@ import com.char1.api.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication // SpringBootServletInitializer needed for .WAR
 public class RestServerApplication extends SpringBootServletInitializer {
+
+	@Override // Method needed for .WAR
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServerApplication.class, args);
