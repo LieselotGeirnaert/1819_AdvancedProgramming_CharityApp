bde80bbe14b1c67ea2505765bb445178672ea05a
sanvacke <Sander.VanAcker@UGent.be>
2018-12-06 10:34:59 +0100

    bracke direct naar master commit

--- spring/src/main/java/com/char1/api/controller/CharityController.java
+++ spring/src/main/java/com/char1/api/controller/CharityController.java
@@ -5,6 +5,7 @@ import com.char1.api.repository.CharityRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +17,20 @@ public class CharityController {
     @Autowired
     CharityRepository charityRepository;
 
-    @RequestMapping(value = "/charity", method = RequestMethod.GET)
-    public Charity charity(@RequestParam(value = "id") String id) {
-        return charityRepository.findById(Integer.parseInt(id));
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String welcome() {
+        return "Welcome";
     }
 
-    @RequestMapping(value = "getAllCharities", method = RequestMethod.GET)
+    @RequestMapping(value = "/charity/{id}", method = RequestMethod.GET)
+    public String charity(@PathVariable String id) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(charityRepository.findById(Integer.parseInt(id)));
+    }
+
+    @RequestMapping(value = "/charity", method = RequestMethod.GET)
     public String getAllCharities() {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(charityRepository.findAll());
     }
-
 }
