391e953c3d01e9861f1455e88374e267ce3f5013
Robin Wijnant <robin.wijnant@student.odisee.be>
2018-12-13 08:24:28 +0100

    Api new challenge

--- spring/src/main/java/com/char1/api/controller/ChallengeController.java
+++ spring/src/main/java/com/char1/api/controller/ChallengeController.java
@@ -4,9 +4,7 @@ import com.char1.api.entity.Challenge;
 import com.char1.api.repository.ChallengeRepository;
 import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,11 @@ public class ChallengeController {
     @GetMapping(value = "/challenge")
     public List<Challenge> getAllChallenges() {
         return challengeRepository.findAll();
+    }
+
+    @PostMapping("/challenge")
+    public Challenge newChallenge(@RequestBody Challenge challenge) {
+        return challengeRepository.save(challenge);
     }
 
 }
