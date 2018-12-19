5cd0f022e0a7d311ace40580f48a11e802374f24
robin.rathe <bobby_rathe@hotmail.com>
2018-12-19 16:56:41 +0100

    Added filter to get challenges by category

--- spring/src/main/java/com/char1/api/repository/ChallengeRepository.java
+++ spring/src/main/java/com/char1/api/repository/ChallengeRepository.java
@@ -1,8 +1,12 @@
 package com.char1.api.repository;
 
+import com.char1.api.entity.Category;
 import com.char1.api.entity.Challenge;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChallengeRepository extends JpaRepository<Challenge, String> {
+public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
     Challenge findById(int id);
+    List<Challenge> findByCategory(Category category);
+
 }
