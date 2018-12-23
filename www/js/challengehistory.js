ff37a84b795083b3a5d8540f8dbddc5c3e7606b8
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-12-23 23:36:54 +0100

    link frontend & backend challenges history

--- www/js/challengehistory.js
+++ www/js/challengehistory.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $.ajax({
-    url: "http://10.129.32.15:8080/user/1/challenge",
+    url: "http://10.129.32.15:8080/userchallenge",
     type: "get",
     dataType: "json",
     beforeSend: function(xhr, settings) {
@@ -8,22 +8,19 @@ $(document).ready(function() {
       xhr.setRequestHeader("Authorization", "Bearer " + access_token);
     },
     success: function(data, textStatus, jqXHR) {
-      $.each(data, function(i, userChallenge) {
+      $.each(data, function(i, challenge) {
         var chall =
-          '<section class="challengetile"><a href="detailchallenge.html?id=' +
-          userChallenge.id +
-          '"><div class="challengetile__info"><img src="' +
-          userChallenge.challenge.linkToLogo +
+          '<section class="challengetile"><img src="' +
+          challenge.challenge.linkToLogo +
           '" alt=""><div class="challengetile__text"><p>' +
-          userChallenge.challenge.category[0].name +
+          challenge.challenge.category[0].name +
           "</p><p>" +
-          userChallenge.challenge.description +
+          challenge.challenge.description +
           "</p><p>" +
-          userChallenge.challenge.unitToMeasure +
+          challenge.challenge.unitToMeasure +
           '</p></div><div class="progressbar"><div class="progressbar__status" style="width:' +
-          userChallenge.progress[userChallenge.progress.length - 1]
-            .currentAmount +
-          '%"></div></div></div></a><div class="challengetile__add"><p>+</p></div></section>';
+          challenge.challenge.progressPercentage +
+          '%"></div></div></section>';
         $("#challenges").append(chall);
       });
     },
