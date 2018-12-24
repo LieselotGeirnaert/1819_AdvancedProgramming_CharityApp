5f7f1496cb635bbfb5d78fb006b7df4ece96cde2
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-12-24 13:03:42 +0100

    css fixes

--- www/js/activechallenges.js
+++ www/js/activechallenges.js
@@ -8,36 +8,36 @@ $(document).ready(function() {
       xhr.setRequestHeader("Authorization", "Bearer " + access_token);
     },
     success: function(data, textStatus, jqXHR) {
-		var completedChallenges = [];
-		$.each(data, function(i, challenge) {
-			if (challenge.completed == true) {
-				completedChallenges.push(challenge);
-			}
-      	});
-		if(completedChallenges.length > 0) {
-			$.each(completedChallenges, function(i, userChallenge) {
-				if(userChallenge.completed == false) {
-					var chall =
-					'<section class="challengetile"><a href="detailchallenge.html?id=' +
-					userChallenge.id +
-					'"><div class="challengetile__info"><img src="' +
-					userChallenge.challenge.linkToLogo +
-					'" alt=""><div class="challengetile__text"><p>' +
-					userChallenge.challenge.category[0].name +
-					"</p><p>" +
-					userChallenge.challenge.description +
-					"</p><p>" +
-					userChallenge.challenge.unitToMeasure +
-					'</p></div><div class="progressbar"><div class="progressbar__status" style="width:' +
-					userChallenge.challenge.progressPercentage +
-					'%"></div></div></div></a><div class="challengetile__add"><p>+</p></div></section>';
-					$("#challenges").append(chall);
-				}	
-			  });
-		} else if (completedChallenges <= 0) {
-			var input = '<div class="nochallenges"><p>Nog geen actieve challenges om weer te geven.</p><p>Maak nu &eacute;&eacute;n aan door op de plus knop te drukken.</p></div>';
-        	$("#challenges").append(input);
-		} 
+      var completedChallenges = [];
+      $.each(data, function(i, challenge) {
+        if (challenge.completed == true) {
+          completedChallenges.push(challenge);
+        }
+      });
+      if (completedChallenges.length > 0) {
+        $.each(completedChallenges, function(i, userChallenge) {
+          if (userChallenge.completed == false) {
+            var chall =
+              '<section class="challengetile"><a href="detailchallenge.html?id=' +
+              userChallenge.id +
+              '"><div class="challengetile__info"><img src="' +
+              userChallenge.challenge.linkToLogo +
+              '" alt=""><div class="challengetile__text"><p>' +
+              userChallenge.challenge.category[0].name +
+              "</p><p>" +
+              userChallenge.challenge.description +
+              "</p><p>" +
+              userChallenge.challenge.unitToMeasure +
+              '</p></div><div class="progressbar"><div class="progressbar__status" style="width:' +
+              userChallenge.challenge.progressPercentage +
+              '%"></div></div></div></a><div class="challengetile__add"><p>+</p></div></section>';
+            $("#challenges").append(chall);
+          }
+        });
+      } else if (completedChallenges <= 0) {
+        var input = "<p>nog geen actieve challenges om weer te geven.</p>";
+        $("#challenges").append(input);
+      }
     },
     error: function(jqXhr, textStatus, errorThrown) {
       //Check if the authentication was invalid, in which case return to index
